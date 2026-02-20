991869cb3d03f3da02910acf602718a5cff90a8b
Sophia Allen <sophiaava03@gmail.com>
2026-02-20 13:35:25 -0500

    lab 3 through add

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -13,6 +13,21 @@ import SimpleArrays from "./SimpleArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex2 from "./FindIndex2";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import Spreader from "./Spreader";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ClientComponentDemo from "./ClientComponentDemo";
+import ServerComponentDemo from "./ServerComponentDemo";
+import Add from "./Add";
 
 export default function Lab3() {
   return (
@@ -34,6 +49,21 @@ export default function Lab3() {
       <ArrayIndexAndLength />
       <AddingAndRemovingToFromArrays />
       <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex2 />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <Spreader />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <ClientComponentDemo />
+      <ServerComponentDemo />
+      <Add a={3} b={4} />
     </div>
   );
 }
