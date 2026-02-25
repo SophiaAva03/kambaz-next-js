23f3d18d05ad7d3ae67c000726d9148d539e7c37
Sophia Allen <sophiaava03@gmail.com>
2026-02-25 15:49:43 -0500

    bug fix 3

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
 import HelloRedux from "./hello/index";
+import store from "../store";
+import { Provider } from "react-redux";
 
 export default function ReduxExamples() {
     return (
-        <div>
-            <h2>Redux Examples</h2>
-            <HelloRedux />
-        </div>
+        <Provider store={store}>
+            <div>
+                <h2>Redux Examples</h2>
+                <HelloRedux />
+            </div>
+        </Provider>
     );
 }
