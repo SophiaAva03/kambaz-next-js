86452a7a73b96e5eac4dad8fa589a3b57422567b
Sophia Allen <sophiaava03@gmail.com>
2026-02-25 14:01:23 -0500

    store bug fix

--- app/labs/lab4/redux/hello/index.tsx
+++ app/labs/lab4/redux/hello/index.tsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 
 export default function HelloRedux() {
-    const { message } = useSelector((state: RootState) => state.helloReducer);
+    const { message } = useSelector((state: RootState) => state.hello);
     return (
         <div id="wd-hello-redux">
             <h3>Hello Redux</h3>
--- app/labs/lab4/store.ts
+++ app/labs/lab4/store.ts
@@ -1,8 +1,16 @@
+// store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../lab4/redux/hello/helloReducer";
 
-const store = configureStore({
-    reducer: { helloReducer }});
-    
+// Create the store with the correct reducer mapping
+export const store = configureStore({
+  reducer: {
+    hello: helloReducer, // ✅ slice name matches exported reducer
+  },
+});
+
+// Types for TypeScript
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export default store;
