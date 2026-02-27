8e63b61ea5ca4046d2c1907e7c4f2925939772e0
Sophia Allen <sophiaava03@gmail.com>
2026-02-27 12:39:17 -0500

    a4 through editing module

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -1,31 +1,40 @@
-import { ReactNode } from "react";
+"use client";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ReactNode, useState } from "react";
 import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa";
-import { courses } from "../../database";
-import Breadcrumb from "./Breadcrumb";
+import { useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { RootState } from "../../store";
 
-export default async function CoursesLayout(
-  { children, params }:
-    Readonly<{ children: ReactNode;
-    params: Promise<{ cid: string }> }>) {
-  const { cid } = await params;
-  const course = courses.find((course) => course._id === cid);
+export default function CoursesLayout({ children }: { children: ReactNode }) {
+  const { cid } = useParams();
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+  const course = courses.find((course: any) => course._id === cid);
+
+  const [showNav, setShowNav] = useState(true);
 
   return (
     <div id="wd-courses">
-        <h2 className="text-danger">
-            <FaAlignJustify className="me-4 fs-4 mb-1" />
-            <Breadcrumb course={course} />
-        </h2> <hr />
-        <div className="d-flex">
-          <div className="d-none d-md-block">
+      <h2>
+        <FaAlignJustify
+          className="me-4 fs-4 mb-1"
+          style={{ cursor: "pointer" }}
+          onClick={() => setShowNav(!showNav)}
+        />
+        {course?.name}
+      </h2>
+      <hr />
+      <div className="d-flex">
+        {showNav && (
+          <div>
             <CourseNavigation />
           </div>
-          <div className="flex-fill">
-              {children}
-          </div>
-        </div>
+        )}
+        <div className="flex-fill">{children}</div>
+      </div>
     </div>
   );
 }
-
